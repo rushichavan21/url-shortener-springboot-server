1cf2b9576bb3a0f0ab9cc044413e150860932ba2
rushichavan21 <rushichavan2327@gmail.com>
2026-01-07 09:17:19 +0530

    feat : init tested via health check. fine to proceed now

---
+++ src/main/java/com/rushi/urlShortener/controllers/PublicController.java
@@ -0,0 +1,14 @@
+package com.rushi.urlShortener.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+    @GetMapping("/health")
+    public String health(){
+        return "Backend Server is live";
+    }
+}
